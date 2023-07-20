2896e57440d26e9466532de74e3d8a4a73ad9bf0
Hussein Alswasy <01096694296hud@gmail.com>
2023-07-20 22:35:49 +0300

    create splash view body

---
+++ lib/Feature/presentation/views/widget/splash_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/Feature/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
