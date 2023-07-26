974a28318fc6c5ccb5c49d663b35b189319dd606
Hussein Alswasy <01096694296hud@gmail.com>
2023-07-27 01:14:11 +0300

    best seller item 3

--- lib/Feature/home/presentation/views/widgets/home_view_body.dart
+++ lib/Feature/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
+import 'package:bookly/Feature/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly/Feature/home/presentation/views/widgets/custome_appBar.dart';
 import 'package:bookly/Feature/home/presentation/views/widgets/feature_list_view_item.dart';
-import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +9,7 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -29,50 +29,6 @@ class HomeViewBody extends StatelessWidget {
           BestSellerListViewItem(),
         ],
       ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          height: 130,
-          child: AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.red,
-                image: const DecorationImage(
-                  image: AssetImage(AssetsData.testimage),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(
-          width: 30,
-        ),
-        Column(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.5,
-              child: const Text(
-                'Harry Potter and the Globet of Fire',
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: Styles.textStyle20,
-              ),
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
