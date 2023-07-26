32048227e11f10c9bb2b75dec269a00c6ec2b0ca
Hussein Alswasy <01096694296hud@gmail.com>
2023-07-26 18:29:29 +0300

    create app styles

--- lib/Feature/home/presentation/views/widgets/home_view_body.dart
+++ lib/Feature/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Feature/home/presentation/views/widgets/custome_appBar.dart';
 import 'package:bookly/Feature/home/presentation/views/widgets/feature_list_view_item.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -20,10 +21,54 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
+          SizedBox(
+            height: 10,
+          ),
+          BestSellerListViewItem(),
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 130,
+          child: AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.red,
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testimage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Column(
+          children: [
+            Text(
+              'Harry Potter',
+              style: TextStyle(fontSize: 25),
+            ),
+            Text(
+              'J.K.Rowling',
+              style: Styles.textStyle18,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/constents.dart
+++ lib/constents.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const KPrimaryColor = Color(0xff100B20);
 
 const KTranstionduration = Duration(milliseconds: 250);
+
+const kGTSetraFine = 'GT Sectra Fine';
