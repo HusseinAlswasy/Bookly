f7ae4e36636255b00dd8093cac1c2988e96605e8
Hussein Alswasy <01096694296hud@gmail.com>
2023-07-30 02:04:03 +0300

    simialr box list view in detailes screen

--- lib/Feature/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Feature/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:bookly/Feature/home/presentation/views/widgets/custom_button.dart';
 import 'package:bookly/Feature/home/presentation/views/widgets/custome_details_book_appbar.dart';
 import 'package:bookly/Feature/home/presentation/views/widgets/custome_list_view_item.dart';
+import 'package:bookly/Feature/home/presentation/views/widgets/similar_box_list_view.dart';
 import 'package:bookly/Feature/home/presentation/views/widgets/title_subtitle_for_detaile_page.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewsBody extends StatelessWidget {
@@ -9,24 +11,43 @@ class BookDetailsViewsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        SizedBox(
+        const SizedBox(
           height: 35,
         ),
-        CustomAppBarBookDetails(),
-        SizedBox(
+        const CustomAppBarBookDetails(),
+        const SizedBox(
           height: 25,
         ),
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 95),
           child: FeatureListViewItem(),
         ),
-        SizedBox(
+        const SizedBox(
           height: 28,
         ),
-        titleAndSubTitileAndRating(),
-        CustomButton(),
+        const titleAndSubTitileAndRating(),
+        const CustomButton(),
+        const Expanded(
+          child: SizedBox(
+            height: 50,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 30),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const SimilarListviewBooks(),
       ],
     );
   }
