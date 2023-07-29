82c459047db681fb20ec72e632cb3d2dc7e2dbab
Hussein Alswasy <01096694296hud@gmail.com>
2023-07-30 02:10:36 +0300

    custom scroll view

--- lib/Feature/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Feature/home/presentation/views/widgets/book_details_view_body.dart
@@ -11,45 +11,53 @@ class BookDetailsViewsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 35,
-        ),
-        const CustomAppBarBookDetails(),
-        const SizedBox(
-          height: 25,
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 95),
-          child: FeatureListViewItem(),
-        ),
-        const SizedBox(
-          height: 28,
-        ),
-        const titleAndSubTitileAndRating(),
-        const CustomButton(),
-        const Expanded(
-          child: SizedBox(
-            height: 50,
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 35,
+              ),
+              const CustomAppBarBookDetails(),
+              const SizedBox(
+                height: 25,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 95),
+                child: FeatureListViewItem(),
+              ),
+              const SizedBox(
+                height: 28,
+              ),
+              const titleAndSubTitileAndRating(),
+              const CustomButton(),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const SimilarListviewBooks(),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 30),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        const SimilarListviewBooks(),
-        const SizedBox(
-          height: 20,
         ),
       ],
     );
