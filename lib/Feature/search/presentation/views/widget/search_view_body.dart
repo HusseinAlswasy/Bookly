751e5bac1b1a960fd57698038c92157ae6e5f641
Hussein Alswasy <01096694296hud@gmail.com>
2023-07-31 00:47:16 +0300

    create custom search view

---
+++ lib/Feature/search/presentation/views/widget/search_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
+
+class CustomeSearchTextFeild extends StatelessWidget {
+  const CustomeSearchTextFeild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
