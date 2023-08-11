292dd655b8e89249c98f9a9f813115f4b6a13cb2
Hussein Alswasy <01096694296hud@gmail.com>
2023-08-11 16:16:56 +0300

    newest book cubit done

---
+++ lib/Feature/home/presentation/manager/newest_books_cubit/newest_books_cubit_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Feature/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Feature/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_cubit_state.dart';
+
+class NewestBooksCubitCubit extends Cubit<NewestBooksCubitState> {
+  NewestBooksCubitCubit(this.homeRepo) : super(NewestBooksCubitInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksCubitLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failuer) {
+      emit(
+        NewestBooksCubitFailuer(failuer.errorMessage),
+      );
+    }, (books) {
+      emit(
+        NewestBooksCubitSuccess(books),
+      );
+    });
+  }
+}
