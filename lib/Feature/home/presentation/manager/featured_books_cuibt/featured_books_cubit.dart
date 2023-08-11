f55ea411327eae3c84d57cf19154360217adaa37
Hussein Alswasy <01096694296hud@gmail.com>
2023-08-11 16:13:07 +0300

    create newest books state

--- lib/Feature/home/presentation/manager/featured_books_cuibt/featured_books_cubit.dart
+++ lib/Feature/home/presentation/manager/featured_books_cuibt/featured_books_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Feature/home/data/models/book_model/book_model.dart';
 import 'package:bookly/Feature/home/data/repos/home_repo.dart';
-import 'package:bookly/core/error/failuers.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
---
+++ lib/Feature/home/presentation/manager/newest_books_cubit/newest_books_cubit_state.dart
@@ -0,0 +1,24 @@
+part of 'newest_books_cubit_cubit.dart';
+
+sealed class NewestBooksCubitState extends Equatable {
+  const NewestBooksCubitState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class NewestBooksCubitInitial extends NewestBooksCubitState {}
+
+final class NewestBooksCubitLoading extends NewestBooksCubitState {}
+
+final class NewestBooksCubitSuccess extends NewestBooksCubitState {
+  final List<BookModel> books;
+
+  const NewestBooksCubitSuccess(this.books);
+}
+
+final class NewestBooksCubitFailuer extends NewestBooksCubitState {
+  final String errorMessage;
+
+  const NewestBooksCubitFailuer(this.errorMessage);
+}
