98507d30f0a3d8420cde60fa827cfa151db99015
Hussein Alswasy <01096694296hud@gmail.com>
2023-08-13 02:42:58 +0300

    add get it for app

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/Feature/home/data/repos/home_repo_implementation.dart';
+import 'package:bookly/core/utils/api_services.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      getIt.get<ApiServices>(),
+    ),
+  );
+}
